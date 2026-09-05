f5ce67bea2e55631fd0e112cc06a5ac97c50029b
MahrukhBaig <mahrukhbaig11@gmail.com>
2026-09-06 00:08:51 +0500

    Complete v0.1.0 release: export Evaluator, package JSON data, polish README, and add Lead Engineer lab note

--- llm_eval_kit/__init__.py
+++ llm_eval_kit/__init__.py
@@ -1,6 +1,13 @@
+from llm_eval_kit.evaluator import Evaluator
+from llm_eval_kit.registry import available_criteria
 from llm_eval_kit.criteria import (
     refusal,
     factual_grounding,
     relevance,
     completeness,
 )
+
+__all__ = [
+    "Evaluator",
+    "available_criteria",
+]
